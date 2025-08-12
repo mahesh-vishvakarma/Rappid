1993cfdc09d0c41e6590c0aaa9de02a3128f9206
Mahi <maheshvishvakarma27@gmail.com>
2025-08-12 11:16:02 +0530

    initialize topbar

--- src/components/Layouts/Topbar.jsx
+++ src/components/Layouts/Topbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
 const Topbar = () => {
   return (
-    <div>
-      Topbar
+    <div className="bg-[#ea2e0e] text-white">
+      <div className="container mx-auto bg-amber-500">
+        <div className="flex">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className="h-5 w-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className="h-5 w-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <RiTwitterXLine className="h-5 w-5" />
+          </a>
+        </div>
+      </div>
+      <div class="container mx-auto bg-amber-200">
+  hello word
+</div>
     </div>
-  )
-}
 
-export default Topbar
+    
+  );
+};
+
+
+
+export default Topbar;
